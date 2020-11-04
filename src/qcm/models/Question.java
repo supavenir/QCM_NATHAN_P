867b65cddaac1684ce963a5fa34831477661d78c
Nathan PRIOU <priounathan@gmail.com>
2020-11-04 17:56:37 +0100

    Alignement avec la proposition de correction

--- src/qcm/models/Question.java
+++ src/qcm/models/Question.java
@@ -3,16 +3,12 @@ package qcm.models;
 public abstract class Question {
 	private String intitule;
 	
-	public Question(String intitule) {
-		this.intitule = intitule;
-	}
-	
-	public abstract boolean estValide(String reponse);
-	
+	public abstract boolean estValide(String response);
+
 	public String getIntitule() {
 		return intitule;
 	}
-	
+
 	public void setIntitule(String intitule) {
 		this.intitule = intitule;
 	}
---
+++ src/qcm/models/QuestionToujoursBonne.java
@@ -0,0 +1,9 @@
+package qcm.models;
+
+public class QuestionToujoursBonne extends Question{
+
+	@Override
+	public boolean estValide(String response) {
+		return true;
+	}
+}
